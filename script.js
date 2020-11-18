a5e460734348c69a5ef89507c268be769315dc97
LennyTheDude <lennyfromrussia@gmail.com>
2020-11-18 23:55:42 +0300

    added logic for all 3 modes & buttons but not connected them

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-let grid = document.getElementById('grid');
-
-for (let row = 0; row < 16; row++) {
-    for (let column = 0; column < 16; column++) {
-        let square = document.createElement('div');
-        square.className = 'item';
-        grid.appendChild(square);
+function drawGrid (size) {
+    grid.innerHTML = "";
+    itemWidth = grid.scrollWidth / size;
+    grid.setAttribute(`style`, `grid-template-columns: repeat(${size}, ${itemWidth}px); grid-template-rows: repeat(${size}, ${itemWidth}px)"`);
+    for (let row = 0; row < size; row++) {
+        for (let column = 0; column < size; column++) {
+            let square = document.createElement('div');
+            square.className = 'item';
+            square.style.width = `${itemWidth}px`;
+            square.style.height = `${itemWidth}px`;
+            square.style.backgroundColor = 'rgba(0, 0, 0, 0.0)';
+            grid.appendChild(square);
+        }
     }
 }
 
-grid.addEventListener('pointerout', event => {
+let grid = document.getElementById('grid');
+let gridSize = 10;
+let itemWidth;
+drawGrid(gridSize);
+
+let mode = 'pure-black';
+
+grid.setAttribute(`style`, `grid-template-columns: repeat(10, ${itemWidth}px); grid-template-rows: repeat(10, ${itemWidth}px)"`);
+
+grid.addEventListener('pointerover', event => { // changing an item's color on mouse over
     if (event.target.className === 'item') {
-        event.target.style.backgroundColor = 'black';
+        switch (mode) {
+            case 'pure-black':
+                event.target.style.backgroundColor = 'black';
+                break;
+            case 'black-gradient':
+                let opacity = parseFloat(event.target.style.backgroundColor.substring(13));
+                if (opacity != NaN) {
+                    opacity += 0.1;
+                    event.target.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+                }
+                break;
+            case 'random-colour':
+                event.target.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+                break;
+            default:
+                break;
+        }
     }
 });
+
